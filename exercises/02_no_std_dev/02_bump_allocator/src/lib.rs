cd43b6c552bf564eb4df061691ebf327fe7c24cb
wesly2000 <ylx.seu@foxmail.com>
2026-03-27 15:37:39 +0800

    fix: multi-thread race

--- exercises/02_no_std_dev/02_bump_allocator/src/lib.rs
+++ exercises/02_no_std_dev/02_bump_allocator/src/lib.rs
@@ -74,16 +74,22 @@ unsafe impl GlobalAlloc for BumpAllocator {
         // 5. Atomically update next to end using compare_exchange
         //    (if CAS fails, another thread raced — retry in a loop)
         // 6. Return the aligned address as a pointer
-        let offset = self.next.load(Ordering::SeqCst);
-        let aligned_offset = (offset + layout.align() - 1) & !(layout.align() - 1);
-        let end = aligned_offset + layout.size();
-        match end {
-            x if x > self.heap_end => null_mut(),
-            x => {
-                _ = self.next.compare_exchange(offset, x, Ordering::SeqCst, Ordering::SeqCst);
-                aligned_offset as *mut u8
+
+        loop {
+            let offset = self.next.load(Ordering::SeqCst);
+            let aligned_offset = (offset + layout.align() - 1) & !(layout.align() - 1);
+            let end = aligned_offset + layout.size();
+            match end {
+                x if x > self.heap_end => return null_mut(),
+                x => {
+                    // Check if another thread also allocates
+                    match self.next.compare_exchange(offset, x, Ordering::AcqRel, Ordering::Acquire) {
+                        Ok(_) => break aligned_offset as *mut u8,
+                        Err(_) => continue
+                    };
+                }
             }
-        }
+        }        
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
